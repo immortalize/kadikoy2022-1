c554b69059375821b40f47cc68ed2373f2bc63ce
Bekir Eldem <bekir.eldem@gmail.com>
2022-11-22 14:27:08 +0300

    Add files via upload

---
+++ JavaScript5/obje.js
@@ -0,0 +1,6 @@
+function ornek (){
+    car.name = "Fiat";
+    car.model = "500";
+    car.weight = "850kg";
+    car.color = "white";
+}
---
+++ JavaScript5/obje1.js
@@ -0,0 +1,8 @@
+const person = {
+  firstName: "John",
+  lastName: "Doe"
+};
+console.log(
+    person.firstName
+    );
+    console.log("test");
